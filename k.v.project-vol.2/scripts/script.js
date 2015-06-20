7884d29fc38d81e1917b7624b04ab56d8db5ba4e
Kristina V <k.valkova@gmail.com>
2015-06-20 14:34:51 +0100

    added second version of my first portfolio project

---
+++ k.v.project-vol.2/scripts/script.js
@@ -0,0 +1,56 @@
+﻿
+/* ******* Header ******* */
+
+$(document).ready(function(){
+	$("#header-wrap").css('backgroundImage', 'url("images/pano_1.jpg")',
+	'backgroundSize', '180px');
+	});
+	$(document).ready(function(){
+	$(".nav").mouseenter(function(){
+	$(this).css({"border-bottom-style":"solid", "border-color":"#3E96D1"});	
+	});
+	$(".nav").mouseleave(function(){
+		$(this).css("border", "none");
+	});
+});
+
+$(document).ready(function(){
+	$(".main-heading").css("textAlign", "center");
+});
+
+/* ******* Projects ******* */
+
+function gitNewTab() {
+	window.open("https://github.com/cocoa1123");
+}
+
+/* ******* Social Icons ******* */
+
+function socialFb() {
+	window.open("https://www.facebook.com/Krissy135");
+}
+function socialGit() {
+	window.open ("https://github.com/cocoa1123");
+}
+function socialLinkedIn() {
+	window.open ("https://www.linkedin.com/profile/view?id=386163268&trk=nav_responsive_tab_profile_pic");
+}
+function socialG() {
+	window.open ("https://plus.google.com/u/0/102626922564967432885/posts");
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
